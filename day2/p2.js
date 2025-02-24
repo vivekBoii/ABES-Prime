bbaccd87903a1bc34ba7964cbc75069e10152298
Vivek <yadav100adu@gmail.com>
2025-02-24 14:32:25 +0530

    flag

--- day2/p2.js
+++ day2/p2.js
@@ -2,7 +2,7 @@ const http= require('http');
 
 const server = http.createServer((req,res)=>{
     res.writeHead(200,{'content-type':'text/html'});
-    res.end('<h1>Vivek Kumar</h1>');
+    res.end('<div style="width: 300px; height: 200px; display: flex; flex-direction: column;"> <div style="background-color: #FFCC00; flex: 1;"></div><div style="background-color: white; flex: 1; position: relative;"> <div style="background-color: #000080; width: 50px; height: 50px; border-radius: 50%; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);"></div></div> <div style="background-color: #008000; flex: 1;"></div></div>');
 })
 
 server.listen(9000,(err)=>{
