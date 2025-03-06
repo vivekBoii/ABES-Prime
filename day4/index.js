0b7e305aeb1f50a84a1836bcda4d5e2883fdac54
Vivek <yadav100adu@gmail.com>
2025-03-06 15:16:03 +0530

    full

---
+++ day4/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/users", (req, res) => {
+  const users = fs.readFileSync("users.json", "utf-8");
+  res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  const users = fs.readFileSync("users.json", "utf-8");
+  const userId = users.length > 0 ? users[users.length - 1].id + 1 : 1;
+  fs.writeFileSync("users.json", JSON.stringify({ ...req.body, id: userId }));
+  res.send("User added successfully");
+});
+
+app.listen(9000, () => {
+  console.log("Server is running on http://localhost:9000");
+});
